a03dd0e74361059ef7810c5959b47b90429d3868
MegaWarl34 <megawarl34@gmail.com>
2026-04-16 15:05:05 +0200

    build gradle and AndroidManifest modified

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,14 +32,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.where_is_it"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId "com.example.where_is_it"
+        minSdkVersion 21 // <--- Modifie ici
+        targetSdkVersion flutter.targetSdkVersion
+                versionCode flutterVersionCode.toInteger()
+        versionName flutterVersionName
     }
 
     buildTypes {
